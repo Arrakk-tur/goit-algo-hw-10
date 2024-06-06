8835287069d8354c36ef5ae6ca573f02a892f5a8
Kostiantyn Skliar <skljar.k@gmail.com>
2024-06-07 00:38:03 +0300

    task 2

---
+++ task_2.py
@@ -0,0 +1,52 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.integrate import quad
+
+# Визначення функції та межі інтегрування
+def f(x):
+    return x ** 2
+
+a = 0  # Нижня межа
+b = 2  # Верхня межа
+
+# Створення діапазону значень для x
+x = np.linspace(-0.5, 2.5, 400)
+y = f(x)
+
+# Створення графіка
+fig, ax = plt.subplots()
+
+# Малювання функції
+ax.plot(x, y, 'r', linewidth=2)
+
+# Заповнення області під кривою
+ix = np.linspace(a, b)
+iy = f(ix)
+ax.fill_between(ix, iy, color='gray', alpha=0.3)
+
+# Налаштування графіка
+ax.set_xlim([x[0], x[-1]])
+ax.set_ylim([0, max(y) + 0.1])
+ax.set_xlabel('x')
+ax.set_ylabel('f(x)')
+
+# Додавання меж інтегрування та назви графіка
+ax.axvline(x=a, color='gray', linestyle='--')
+ax.axvline(x=b, color='gray', linestyle='--')
+ax.set_title('Графік інтегрування f(x) = x^2 від ' + str(a) + ' до ' + str(b))
+plt.grid()
+plt.show()
+
+# Метод Монте-Карло для обчислення інтегралу
+N = 10000  # Кількість випадкових точок
+random_x = np.random.uniform(a, b, N)
+random_y = f(random_x)
+
+# Обчислення середнього значення функції на випадкових точках
+integral_value_mc = (b - a) * np.mean(random_y)
+print(f"Оцінене значення інтегралу методом Монте-Карло: {integral_value_mc}")
+
+# Обчислення точного значення інтегралу за допомогою функції quad
+integral_value_quad, error = quad(f, a, b)
+print(f"Точне значення інтегралу за допомогою функції quad: {integral_value_quad}")
+print(f"Похибка методу Монте-Карло: {abs(integral_value_mc - integral_value_quad)}")
